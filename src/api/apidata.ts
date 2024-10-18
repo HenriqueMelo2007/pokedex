cac336c44c1550a1cabd2b5e4440242f9ccb263e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-10-18 14:43:16 -0300

    feat: many

---
+++ src/api/apidata.ts
@@ -0,0 +1,8 @@
+const url = 'https://pokeapi.co/api/v2/'
+
+export default async function getAll() {
+  const response = await fetch(url + 'pokemon?limit=100000&offset=0.').then(
+    response => response.json()
+  )
+  return response.results;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import poke from '@/public/images/poke.png'
 import pikachuPNG from '@/public/images/pkcPNG.png'
+
+import Input from '@/components/input'
 import PokeGrid from '@/components/pokegrid'
 
 export default function Home() {
@@ -20,11 +22,8 @@ export default function Home() {
           alt="Pikachu img"
           className="absolute mr-mPKC"
         ></Image>
-        <input
-          type="text"
-          placeholder="Search for a Pokemon"
-          className="rounded-xl outline-none h-12 w-80 text-base p-2 pl-10 border-2 border-yellow-300 bg-blue-800 text-yellow-300 placeholder:text-yellow-300"
-        />
+
+        <Input></Input>
         <PokeGrid></PokeGrid>
       </div>
     </div>
